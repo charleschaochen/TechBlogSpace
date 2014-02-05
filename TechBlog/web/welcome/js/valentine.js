cb0436171beb0cbb57283e051e2e103053f900ad
charleschaochen <399931011@qq.com>
2014-02-05 16:08:08 +0800

    Modify valentine's day welcome page

--- TechBlog/web/welcome/js/valentine.js
+++ TechBlog/web/welcome/js/valentine.js
@@ -9,11 +9,64 @@ $(function () {
      * Light up the light
      */
     $(".light").click(function () {
+        // Change light level
         var color = $(this).attr("class").replace("light ", "");
         var level = parseInt($(this).attr("alt")) + 1;
         if (level >= 4)level = 0;
         var pic = color + "_" + level + ".png";
         $(this).css("background", "url(/welcome/images/lights/" + pic + ")");
         $(this).attr("alt", level);
+
+        // Change opacity of the main content
+        var opacity = parseFloat($(".mask_layer").css("opacity"));
+        if (opacity > 0.1) {
+            opacity -= 0.1;
+            $(".mask_layer").css("opacity", opacity);
+        } else {
+            $(".mask_layer").css("opacity", 0);
+        }
     });
+
+
+    disable_scroll();
 });
+
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = [37, 38, 39, 40];
+
+function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault)
+        e.preventDefault();
+    e.returnValue = false;
+}
+
+function keydown(e) {
+    for (var i = keys.length; i--;) {
+        if (e.keyCode === keys[i]) {
+            preventDefault(e);
+            return;
+        }
+    }
+}
+
+function wheel(e) {
+    preventDefault(e);
+}
+
+function disable_scroll() {
+    if (window.addEventListener) {
+        window.addEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = wheel;
+    document.onkeydown = keydown;
+}
+
+function enable_scroll() {
+    if (window.removeEventListener) {
+        window.removeEventListener('DOMMouseScroll', wheel, false);
+    }
+    window.onmousewheel = document.onmousewheel = document.onkeydown = null;
+}
