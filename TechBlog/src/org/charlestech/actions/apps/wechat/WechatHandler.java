3c10523ccb5204522ce598a8fdf6e55033ae4b8c
charleschaochen <399931011@qq.com>
2014-02-19 18:48:12 +0800

    Modify wechat handler

--- TechBlog/src/org/charlestech/actions/apps/wechat/WechatHandler.java
+++ TechBlog/src/org/charlestech/actions/apps/wechat/WechatHandler.java
@@ -3,12 +3,22 @@ package org.charlestech.actions.apps.wechat;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.commons.lang.StringUtils;
 import org.apache.struts2.ServletActionContext;
+import org.charlestech.beans.wechat.WechatProcessor;
+import org.charlestech.beans.wechat.WechatUtils;
+import org.charlestech.po.wechat.Message;
+import org.charlestech.po.wechat.TextMessage;
 import org.charlestech.utils.SHA1;
+import org.dom4j.DocumentException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
 
 /*
  * @Description Interact with Wechat Open Platform
@@ -25,16 +35,38 @@ public class WechatHandler extends ActionSupport {
     private final String TOKEN = "charleschaochen";
     /* End: Verification parameters */
 
+    private WechatProcessor wechatProcessor;    // Wechat processor
+
+    public void setWechatProcessor(WechatProcessor wechatProcessor) {
+        this.wechatProcessor = wechatProcessor;
+    }
+
+
     /**
      * Default execute
      *
      * @return
      */
     public String execute() throws IOException {
+        HttpServletRequest request = ServletActionContext.getRequest();
         HttpServletResponse response = ServletActionContext.getResponse();
         response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = out = response.getWriter();
-        verify(out);
+        Message message;
+
+        try {
+            message = WechatUtils.retrieveMessage(request); // Read message
+            String type = message.getType();
+            /* Start: Process message according to the type */
+            // Text message
+            if (StringUtils.equals(type, WechatUtils.TEXT)) {
+                wechatProcessor.proccessText((TextMessage) message.getMess(), response.getWriter());
+                return null;
+            }
+            /* End: Process message according to the type */
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 
@@ -88,4 +120,5 @@ public class WechatHandler extends ActionSupport {
     public String getTOKEN() {
         return TOKEN;
     }
+
 }
